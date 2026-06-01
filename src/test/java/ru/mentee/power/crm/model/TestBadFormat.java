dcd1c8bad32b26ca0de869e5f5962091d813b01a
Gleb Druzhinin <lizardgd99@gmail.com>
2026-06-01 21:31:05 +0300

    BCORE-34-Test+Final: Проверка Spotless
    - Добавил тест с 4-мя пропусками

---
+++ src/test/java/ru/mentee/power/crm/model/TestBadFormat.java
@@ -0,0 +1,7 @@
+package ru.mentee.power.crm.model;
+
+public class TestBadFormat {
+    public void badIndent() {        // ❌ 4 пробела (должно быть 2)
+        System.out.println("test");
+    }
+}
